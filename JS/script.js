5934774af830b321830823b674e8dd64df41a705
Haiquel R. Lawreniuk <haiquelcita@gmail.com>
2022-12-07 16:56:47 -0300

    Cambios P.O.E.C

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+"use strict"
+const ul = document.querySelector("ul");
+
+function frames() {
+    const animacion = ul.animate([
+        //keyframes
+        { transform: "translateY(0px)" },
+        { transfomr: "translateY(-100px)" }
+    ], {
+        //opciones
+        //velocidad:
+        easing: "linear",
+        iterations: 1,
+        duration: 500 //milisegundos
+    });
+    return animacion.finished;
+}
+
+function displace() {
+    frames()
+        .then((res) => {
+            console.log(res);
+            ul.appendChild(document.querySelectorAll("ul>li")[0]) //aprenChild coloca un hijo al elemento 
+        })
+}
+
+//setInterval repite operaciones cada cierto tiempo determinado.
+setInterval(() => {
+    displace();
+}, 1000);
