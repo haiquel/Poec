701ffd6bc46ca62548e0b44722bad8ada8bf2b98
Haiquel R. Lawreniuk <108825789+haiquel@users.noreply.github.com>
2022-12-12 14:08:40 -0300

    Defining functions

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,24 @@
 "use strict"
 const ul = document.querySelector("ul");
+
+function frames() {
+    const animacion = ul.animate([
+        //keyframes
+        { transform: "translateY(0px)" },
+    ], {
+        //opciones
+        //velocidad:
+        easing: "linear",
+        iterations: 1,
+        duration: 500 //milisegundos
+    });
+    return animacion.finished;
+}
+
+function displace() {
+    frames()
+        .then((res) => {
+            console.log(res);
+            ul.appendChild(document.querySelectorAll("ul>li")[0]) //aprenChild coloca un hijo al elemento 
+        })
+}
